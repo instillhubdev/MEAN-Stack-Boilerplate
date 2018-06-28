d117529218d3c467b1fce1a722bca2c5f6a32e78
prateek951 <prince.cenation@gmail.com>
2018-06-28 14:09:01 +0530

    mean stack boilerplate

---
+++ config/emails.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const emails = {
+    admin: "prince.cenation@gmail.com",
+
+    verification: {
+        subject: "Verify your Email",
+        htmlBody: ""
+    },
+    password: {
+        subject: "Reset your Password",
+        htmlBody: ""
+    },
+    signupAdmin: {
+        subject: "New user registered",
+        htmlBody: ""
+    }
+};
+
+module.exports = emails;
